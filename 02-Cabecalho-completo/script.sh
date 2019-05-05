ed5b3e5c8f4223eb69cd96336ffacc95b5abea66
José Malcher Jr <contato@josemalcher.net>
2019-05-04 23:29:41 -0300

    02-Cabeçalho completo

---
+++ 02-Cabecalho-completo/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# script.sh - Meu primeiro script
+#
+# Autor: José Malcher JR. <contato@josemalcher.net>
+#
+# ---------------------------------------------------------------------
+# Esse programa recebe como parâmetro o nome de usuário e retorna
+# o nome completo do usuário na saída padrão.
+#
+# Exemplo:
+# 	$ ./script.sh Jose
+#	José Malcher Jr.
+#
+#
+# Histórico:
+# 	v1.0 2016-02-26, José Malcher Jr.:
+#		- Versão melhorada no quesito X
+#
+# Licença: GPL
